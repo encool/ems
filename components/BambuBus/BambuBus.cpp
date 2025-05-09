b4ccfdcbe061d5122cd92d829b1187a099d66d00
qiao <liqiao1000@gmail.com>
2025-05-09 17:33:11 +0800

    1

--- components/BambuBus/BambuBus.cpp
+++ components/BambuBus/BambuBus.cpp
@@ -1186,7 +1186,7 @@ void BambuBus::send_uart_with_de(const uint8_t *data, uint16_t length)
         this->de_pin_->digital_write(true); // 激活发送 (高电平)
         // 可能需要极短的延迟确保收发器状态切换 (通常非常快)
         esphome::delayMicroseconds(10); // 示例: 5 微秒，根据硬件调整
-        ESP_LOGV(TAG, "DE pin set HIGH.");
+        ESP_LOGD(TAG, "DE pin set HIGH.");
     }
     else
     {
@@ -1211,6 +1211,6 @@ void BambuBus::send_uart_with_de(const uint8_t *data, uint16_t length)
         // 在禁用 DE 之前可能需要短暂延迟，确保最后一个停止位完全发出
         esphome::delayMicroseconds(10);      // 示例: 5 微秒，根据硬件调整
         this->de_pin_->digital_write(false); // 禁用发送 (低电平)
-        ESP_LOGV(TAG, "DE pin set LOW.");
+        ESP_LOGD(TAG, "DE pin set LOW.");
     }
 }
